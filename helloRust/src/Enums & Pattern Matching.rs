7c335c88720a77ddd1caeda5006edde30bc237a3
Anadi-Gupta1 <166429589+Anadi-Gupta1@users.noreply.github.com>
2025-08-26 08:51:45 +0530

    Update Enums & Pattern Matching.rs: 2025-08-26 08:51:45

--- helloRust/src/Enums & Pattern Matching.rs
+++ helloRust/src/Enums & Pattern Matching.rs
@@ -16,3 +16,27 @@ fn main() {
     let c = Shape::Circle(3.0);
     println!("Area: {}", area(c));
 }
+
+
+
+
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+fn move_player(dir: Direction) {
+    match dir {
+        Direction::North => println!("Moving up"),
+        Direction::South => println!("Moving down"),
+        Direction::East => println!("Moving right"),
+        Direction::West => println!("Moving left"),
+    }
+}
+
+fn main() {
+    let dir = Direction::North;
+    move_player(dir);
+}
