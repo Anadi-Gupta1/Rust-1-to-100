355d6fb839789a446ec1b4d63fcd2654d2d04649
Anadi-Gupta1 <166429589+Anadi-Gupta1@users.noreply.github.com>
2025-09-01 09:08:25 +0530

    Update: 2025-09-01 09:08:25

--- helloRust/src/Enums & Pattern Matching.rs
+++ helloRust/src/Enums & Pattern Matching.rs
@@ -20,6 +20,8 @@ fn main() {
 
 
 
+
+
 enum Direction {
     North,
     South,
@@ -56,3 +58,41 @@ fn get_direction name(dir: Direction) -> &"static str"{
         Direction::west => "west",
     }
 }
+
+
+
+// What is pattern matching
+pattern matching is a type of matching in which the structure and content of data are examined to determine its type and extract relevant information. In Rust, pattern matching is commonly used with enums and can be done using the `match` keyword.
+Pattern matching allows you to concisely and safely handle different data types and structures, making your code more readable and maintainable.
+Pattern matching matches across various variants and calculates the appropriate response based on the specific variant being matched.
+
+
+
+
+enum Shape {
+    Circle(f64),       // radius
+    Rectangle(f64, f64), // width, height
+    Square(f64)
+}
+
+
+}
+fn calculate_area(shape: Shape) -> f64 {
+    match shape {
+        Shape::Circle(radius) => 3.14*radius*radius,
+        Shape::Rectangle(width, height) => width*height,
+        Shape::Square(side_length) => side_length*side_length,
+    }
+}
+fn main() {
+    let shape = Shape::Circle(5.0);
+    println!("Area: {}", calculate_area(shape));
+    let shape = Shape::Square(4.0);
+    println!("Area: {}", calculate_area(shape));
+    let shape = Shape::Rectangle(3.0, 6.0);
+    println!("Area: {}", calculate_area(shape));
+}
+return 0;
+}
+
+    Pattern matching gives you all the values and expect you to give all the values so you must handle every possible variant.
